828f23f5eb5656dea0e438943354313503bdf70b
PK <pknw1@users.noreply.github.com>
2023-05-22 08:15:41 +0100

    f

--- expired.sh
+++ expired.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+timestamp=$(date -u +%d/%m/%Y)
+
 DEPLOYUMENT=$(az group list --query "[?tags.expires<=\`${timestamp}\`]".id -o tsv )
-echo $DEPLOYUMENT
+echo $DEPLOYUMENT >> ./deployment
