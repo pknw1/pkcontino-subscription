2b4f2c01451fb07c5813ddc08fa1bd70064296d1
PK <pknw1@users.noreply.github.com>
2023-05-22 08:24:37 +0100

    d

--- expired.sh
+++ expired.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 timestamp=$(date -u +%d/%m/%Y)
 
-DEPLOYUMENT=$(az group list --query "[?tags.expires<=\`${timestamp}\`]".id -o tsv | head -n1 |awk -F '{print $NF}')
+DEPLOYUMENT=$(az group list --query "[?tags.expires<=\`${timestamp}\`]".id -o tsv | head -n1 |awk -F/ '{print $NF}')
 echo $DEPLOYUMENT 
