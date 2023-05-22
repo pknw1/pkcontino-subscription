371e033437e777a8038935f7325f83639336e955
PK <pknw1@users.noreply.github.com>
2023-05-22 08:09:01 +0100

    fic

--- expired.sh
+++ expired.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DEPLOYUMENT=$(az group list --query "[?tags.expires<=\`${timestamp}\`]".id -o tsv | awk -F/ '{print $NF}'/ )
+DEPLOYUMENT=$(az group list --query "[?tags.expires<=\`${timestamp}\`]".id -o tsv | awk -F/ \'{print $NF}\'/ )
 echo $DEPLOYMENT
