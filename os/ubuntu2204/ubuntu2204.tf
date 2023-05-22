88c581890836cdf4760e51865e54a68eb50708af
PK <pknw1@users.noreply.github.com>
2023-05-22 13:58:05 +0100

    fix

--- os/ubuntu2204/ubuntu2204.tf
+++ os/ubuntu2204/ubuntu2204.tf
@@ -1,16 +1,18 @@
 variable "expiry" { default = 1 }
 
+
 locals {
   current_time           = timestamp()
   today                  = formatdate("YYYY-MM-DD", local.current_time)
-    
-  max_start_date         = formatdate("YYYY-MM-DD", timeadd(local.today, "1d"))
+  hours                  = var.expiry * 24
+  max_start_date         = formatdate("YYYY-MM-DD", timeadd(timestamp(), "${local.hours}h"))
 }
+
 resource "azurerm_resource_group" "rg" {
   name     = "ubuntu2204"
   location = "uksouth"
   tags     = { 
-    expires =  "testval"
+    expires =  local.max_start_date
     }    
 }
 
